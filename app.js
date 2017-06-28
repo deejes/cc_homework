2f00f23ba08447000a9271d89ec55d062702043e
Steve Godin <steve.god@gmail.com>
2017-06-28 10:51:15 -0700

    Create custom logger middleware

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
+// Step 1: Create project with `yarn init`
+// Step 2: Add Express package with `yarn add express`
+// Step 3: Create `app.js` file
+// Step 4: Require Express in `app.js`
+// Step 5: Create a route to serve `/hello/:name`
+// Step 6: Have server, app, listen on PORT
+// Step 7: Install nodemon & add start script
+
 const express = require('express');
 
 // Calling the express function will create
 // an instance of a Express web server
 const app = express();
+
+// unlike app.get, app.use will match for all HTTP verbs
+// If we do not give a path as the first argument, it will match for
+// every URL
+app.use(function (request, response, next) {
+  console.log(`📝 ${request.method} – ${request.path} – ${new Date().toString()}`);
+  // next, a function and third argument of a middleware callback,
+  // express to move on to the next middleware
+  next();
+  // If you do not call next, or terminate a response with response.send,
+  // the browser will appear to never stop loading when accessing a URL
+  // on your server
+});
 
 // making a part of the relative url begin with :, will make
 // that matched section available as data in request.params under a
