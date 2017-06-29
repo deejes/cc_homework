8ef840b6db015090698f43928f8abc13a20f2a5c
Steve Godin <steve.god@gmail.com>
2017-06-29 11:45:08 -0700

    Move app.js routes to routes/home.js

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
+const home = require('./routes/home');
+
 // Calling the express function will create
 // an instance of a Express web server
 const app = express();
@@ -52,59 +54,9 @@ app.use(function (req, res, next) {
   next();
 });
 
-// making a part of the relative url begin with :, will make
-// that matched section available as data in request.params under a
-// property named after the word that follows :.
-
-// URL: http://localhost:4545/hello/:name HTTP VERB: GET
-app.get('/hello/:name', function (request, response) {
-  // This function is named Middleware
-  // It's passed the arguments in order: request, response & next
-  // - request is an object that contains the entire message from
-  //   client. This means http headers, any data its sending, etc.
-  // - response is an object that contains the message our server will
-  //   reply with to the client. It also contains http headers, our own data, etc.
-  const name = request.params.name || 'World';
-  response.send(`Hello, ${name}!`);
-});
-
-// URL: http://localhost:4545/ HTTP VERB: GET
-app.get('/', function (request, response) {
-  // response.render's first argument is the location and name
-  // of a template we want render beginning at views/
-  response.render('index');
-});
-
-// URL: http://localhost:4545/contact HTTP VERB: GET
-app.get('/contact', function (request, response) {
-  response.render('contact', {contact: {}});
-});
-
-// URL: http://localhost:4545/contact HTTP VERB: POST
-app.post('/contact', function (request, response) {
-  // when a form post is parsed by bodyParser,
-  // its data is formatted as a javascript object and it
-  // assigned to the body property of request
-  response.render('contact', {contact: request.body});
-});
-
-// URL: http://localhost:4545/username HTTP VERB: POST
-app.post('/username', function (req, res) {
-  const {username} = req.body;
-  // 👆 this syntax is named: destructuring. It's a shortcut (syntax sugar)
-  // for the code below
-  // const username = req.body.username;
-  // res.cookie is method added by the cookie-parser
-  // its convenient way to set cookies in the header of the response
-  // - the first argument is the name of the cookie
-  // - the second argument is the value of the cookie
-  // - the third and last argument is object to configure the cookie
-  res.cookie('username', username, {maxAge: 1000*60*60*24});
-  // the maxAge option determines how long a cookie will last in milliseconds
-
-  // res.redirect tells the browser, in the response, to go to the given URL
-  res.redirect('/');
-});
+// When use app.use to mount a router file, the first argument
+// is a string that will prefix all uri paths defined inside the router file
+app.use('/', home);
 
 // PORT is uppercased because we intend to be a constant.
 // It shouldn't be changed after it's declared.
